8a57d05b7ed1cd2eaa10bf0f1753aca6883ea263
stalv <stalv@VALENTIN-PC>
2020-09-21 00:17:45 +0400

    Add blog

---
+++ blogKlimov/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogKlimov.Controllers
+{   
+    /// <summary>
+    /// Контроллер для работы с блогом
+    /// </summary>
+    public class BlogController : Controller
+    {
+        /// <summary>
+        /// GET Возвращает представление страницы Blog (index.cshtml)
+        /// </summary>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ blogKlimov/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using blogKlimov.Models;
+
+namespace blogKlimov.Controllers
+
+{   /// <summary>
+    /// Контроллер для работы с главной страницей и политикой конфиденциальности
+    /// </summary>
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        /// <summary>
+        /// Констурктор класса <see cref="HomeController"/>
+        /// </summary>
+        /// <param name="logger">logger</param>
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// GET Возвращает представление страницы Home (index.cshtml)
+        /// </summary>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// GET Возвращает представление страницы Privacy (privacy.cshtml)
+        /// </summary>
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// GET Возвращает ошибку при неудачном запросе Shared (Error.cshtml) 
+        /// </summary>
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
